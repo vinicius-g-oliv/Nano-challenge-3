c387f103f1137577c099b316413735b17b39be28
Vinicius Gomes <78090044+vinicius-g-oliv@users.noreply.github.com>
2022-03-31 14:58:27 -0300

    Upload projeto
    Modificado gitignore e gitattributes.

---
+++ Teste_Academy/Teste_Academy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Teste_Academy
+//
+//  Created by Vinicius Gomes on 31/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
