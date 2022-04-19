aa05e95b43ee2eda5c0025493cb87a251e77381e
Vinicius Gomes <78090044+vinicius-g-oliv@users.noreply.github.com>
2022-04-19 17:42:58 -0300

    Adicionado mais uma tela

---
+++ NanoChallenge3/NanoChallenge3/Caderno3ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  Caderno3ViewController.swift
+//  NanoChallenge3
+//
+//  Created by Vinicius Gomes on 19/04/22.
+//
+
+import UIKit
+
+class Caderno3ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "Matemática e Ciências da Natureza"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- NanoChallenge3/NanoChallenge3/ViewController.swift
+++ NanoChallenge3/NanoChallenge3/ViewController.swift
@@ -4,15 +4,22 @@
 //
 //  Created by Vinicius Gomes on 07/04/22.
 //
-
+import iCarousel
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var btnCaderno1: UIButton!
     
+    @IBOutlet weak var btnCaderno3: UIButton!
+    @IBOutlet weak var btnCaderno2: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Cadernos"
-        
+        let titulo = "Your Test"
+        title = titulo.uppercased()
+        btnCaderno1.layer.cornerRadius = 12
+        btnCaderno2.layer.cornerRadius = 12
+        btnCaderno3.layer.cornerRadius = 12
+    
     }
 
 
