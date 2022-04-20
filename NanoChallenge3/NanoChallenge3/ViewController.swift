51c433183e554d8cfe1e3d5bb2475c09c06802e7
Guilborges <Lozanoguilherme@gmail.com>
2022-04-20 14:32:19 -0300

    Date Picker

--- NanoChallenge3/NanoChallenge3/ViewController.swift
+++ NanoChallenge3/NanoChallenge3/ViewController.swift
@@ -9,11 +9,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var birthDateTxt: UITextField!
+    
+    let datePicker = UIDatePicker()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        createDatePicker()
     }
 
-
+    func createDatePicker(){
+        
+        birthDateTxt.textAlignment = .center
+        
+        //toolbar
+        let  toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        
+        // bar button
+        let doneBtn = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(donePressed))
+        toolbar.setItems([doneBtn], animated: true)
+        
+        //assign toolbar
+        birthDateTxt.inputAccessoryView = toolbar
+        
+        //assign date picker to the text field
+        birthDateTxt.inputView = datePicker
+        
+        //date picker mode
+        datePicker.datePickerMode = .date
+        
+        //Tamanho do datePicker
+        datePicker.preferredDatePickerStyle = .wheels
+    }
+    
+    @objc func donePressed(){
+        //formatter
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .none
+        
+        birthDateTxt.text = formatter.string(from: datePicker.date)
+        self.view.endEditing(true)
+    }
 }
 
