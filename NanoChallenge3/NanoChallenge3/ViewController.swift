6100626de5433e15821c71c1169b6cca6b9cc795
Guilborges <Lozanoguilherme@gmail.com>
2022-04-27 17:48:15 -0300

    CarouselFuncionando

--- NanoChallenge3/NanoChallenge3/ViewController.swift
+++ NanoChallenge3/NanoChallenge3/ViewController.swift
@@ -7,21 +7,25 @@
 
 import UIKit
 
-class ViewController: UIViewController{
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource{
+    
    
-    @IBOutlet weak var CollectionViewIB: UICollectionView!
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var pageControl: UIPageControl!
     @IBOutlet weak var btnCaderno1: UIButton!
     @IBOutlet weak var btnCaderno3: UIButton!
     @IBOutlet weak var btnCaderno2: UIButton!
     
+    var imgCar = [UIImage(named: "foto 1")!,UIImage(named: "foto 2")!,UIImage(named: "foto 3")!]
     var timer : Timer?
-    
-    var imagensEnem = [UIImage(named: "ENEM1")!,UIImage(named: "ENEM2")!,UIImage(named: "ENEM3")]
-var currentCellIndex = 0
-    
+    var currentCellIndex = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-      
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        pageControl.numberOfPages = imgCar.count
+        
+        startTimer()
         let titulo = "Your Test"
         title = titulo.uppercased()
         
@@ -34,7 +38,45 @@ var currentCellIndex = 0
         
   
     }
- 
+
+    
+    func startTimer(){
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(moveToNextIndex), userInfo: nil, repeats: true)
+    }
+    
+    @objc func moveToNextIndex(){
+        if currentCellIndex < imgCar.count - 1{
+            currentCellIndex += 1
+        }else{
+            currentCellIndex = 0
+        }
+        
+        collectionView.scrollToItem(at: IndexPath(item: currentCellIndex, section: 0), at: .centeredHorizontally, animated: true)
+        pageControl.currentPage = currentCellIndex
+       
+     }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return imgCar.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "caroCell", for: indexPath) as! CarouselCollectionViewCell
+    
+        cell.imgCarousel.image = imgCar[indexPath.row]
+        return cell
+        
+    }
+    
+   func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+
     
    
 }
