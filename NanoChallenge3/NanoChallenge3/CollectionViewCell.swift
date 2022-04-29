e5cb775f8b35301fbbcef54ab516e0dd2e96d949
Vinicius Gomes <78090044+vinicius-g-oliv@users.noreply.github.com>
2022-04-29 11:50:58 -0300

    Revert "Revert "Merge branch 'bkp3' into bkpmain""
    This reverts commit 407daf1bf2a7833f2b910af46f923a4b3e8fb94e.

---
+++ NanoChallenge3/NanoChallenge3/CollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionViewCell.swift
+//  NanoChallenge3
+//
+//  Created by Vinicius Gomes on 20/04/22.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+
+    @IBOutlet weak var myImage: UIImageView!
+    
+ 
+    
+}
---
+++ NanoChallenge3/NanoChallenge3/MyCollection.swift
@@ -0,0 +1,29 @@
+//
+//  MyCollection.swift
+//  NanoChallenge3
+//
+//  Created by Vinicius Gomes on 20/04/22.
+//
+
+import UIKit
+
+class MyCollection: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
