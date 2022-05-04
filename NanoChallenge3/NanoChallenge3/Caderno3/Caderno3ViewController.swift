54f231c6eeb9728932a6134318e6483bca9d9074
Vinicius Gomes <78090044+vinicius-g-oliv@users.noreply.github.com>
2022-05-04 16:59:56 -0300

    Personalizado a celula card3

--- NanoChallenge3/NanoChallenge3/Caderno3/Caderno3ViewController.swift
+++ NanoChallenge3/NanoChallenge3/Caderno3/Caderno3ViewController.swift
@@ -16,13 +16,20 @@ class Caderno3ViewController: UIViewController, UITableViewDelegate, UITableView
     
     @IBOutlet weak var Novoregistro: UIButton!
     var modelo: [(title: String, anotacao: String, btndata: String)] = []
+    
     let dataPicker = UIDatePicker()
+    
     override func viewDidLoad() {
+        
+        let nib  = UINib(nibName: "CustomCellTableViewCell", bundle: nil)
+        table.register(nib, forCellReuseIdentifier: "CustomCellTableViewCell")
+        
         super.viewDidLoad()
         super.viewDidLoad()
         table.delegate = self
         table.dataSource = self
         title = "Matemática e Ciências da Natureza"
+        table.reloadData()
         
         
     }
@@ -59,21 +66,21 @@ class Caderno3ViewController: UIViewController, UITableViewDelegate, UITableView
    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return 10
     }
-    func tableView(_ tableView: UITableView, numbersOfRowsInSection section: Int) -> Int {
-        return 1
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50.0
     }
+
+
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell3", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CustomCellTableViewCell", for: indexPath) as! CustomCellTableViewCell
        
-        cell.textLabel?.text = modelo[indexPath.section].title
-        cell.detailTextLabel?.text = modelo[indexPath.section].anotacao
+        cell.nota.text = modelo[indexPath.section].title
+        cell.data.text = modelo[indexPath.section].btndata
+       
+        
         return cell
     }
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-          modelo.remove(at: indexPath.section)
-          tableView.deleteRows(at: [indexPath], with: .automatic)
-      }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
---
+++ NanoChallenge3/NanoChallenge3/Caderno3/CustomCellTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  CustomCellTableViewCell.swift
+//  NanoChallenge3
+//
+//  Created by Vinicius Gomes on 04/05/22.
+//
+
+import UIKit
+
+class CustomCellTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var nota: UILabel!
+    @IBOutlet weak var fundo: UIView!
+    @IBOutlet weak var data: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        self.fundo.layer.cornerRadius = 10
+        self.fundo.layer.borderWidth = 0
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
