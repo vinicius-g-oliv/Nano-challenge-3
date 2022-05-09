885616ef059c5377bed7cff2064f1f7c129dc1ac
Vinicius Gomes <78090044+vinicius-g-oliv@users.noreply.github.com>
2022-05-09 14:11:50 -0300

    Revert "Revert "User default""
    This reverts commit 673c6c245624245f273bdf86d815b93029aac4a6.

---
+++ NanoChallenge3/NanoChallenge3/Caderno3/Registro.swift
@@ -0,0 +1,19 @@
+//
+//  Registro.swift
+//  NanoChallenge3
+//
+//  Created by Vinicius Gomes on 06/05/22.
+//
+
+import Foundation
+
+struct Registro : Codable {
+    var nota: String
+    var data: String
+    
+    public init(nota: String, data: String){
+        self.nota = nota
+        self.data = data
+        
+    }
+}
